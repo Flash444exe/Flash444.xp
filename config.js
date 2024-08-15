d6a34ed0f41a0b9924d0ceff2c7a62b1416973ca
Flash444exe <flaaashzzzz@gmail.com>
2024-08-15 23:37:32 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,20 +9,20 @@ global.setting = {
  }
 
 global.owner = [
-['212717457920', 'Nour', true],
-['212605784394', 'silana', false],
+['212717457920', 'flash', true],
+['212605784394', 'flx', false],
 ['', '', false]
 ]
 
 global.info = {
- nomerbot: '212605784394',
+ nomerbot: '212614515664',
  pairingNumber: '212693227969',
  nameown: 'Nour',
- nomerown: '212717457920',
+ nomerown: '212714249761',
  packname: 'sticker by ',
- author: 'Noureddine',
+ author: 'Flash',
  namebot: 'ꜱɪʟᴀɴᴀ',
- wm: ''-'_꩜ 𝘚𝘪𝘭𝘢𝘯𝘢 𝘣𝘰𝘵 ꩜_'-'',
+ wm: ''-'_꩜ Flx 𝘣𝘰𝘵 ꩜_'-'',
  stickpack: 'Whatsapp',
  stickauth: 'ꜱɪʟᴀɴᴀ ʙᴏᴛ '
 }
@@ -32,9 +32,9 @@ global.media = {
  profil: 'https://i.ibb.co/3Fh9V6p/avatar-contact.png',
  did: 'https://telegra.ph/file/fdc1a8b08fe63520f4339.jpg',
  rules: 'https://telegra.ph/file/afcfa712bd09f4fcf027a.jpg',
- thumbnail: 'https://telegra.ph/file/161c0a22c03f7859c7599.jpg',
- thumb: 'https://telegra.ph/file/161c0a22c03f7859c7599.jpg',
- logo: 'https://telegra.ph/file/161c0a22c03f7859c7599.jpg',
+ thumbnail: 'https://www.imghippo.com/i/hf7tS1723758369.png',
+ thumb: 'https://www.imghippo.com/i/hf7tS1723758369.png',
+ logo: 'https://www.imghippo.com/i/hf7tS1723758369.png',
  unReg: 'https://telegra.ph/file/ef02d1fdd59082d05f08d.jpg',
  registrasi: 'https://telegra.ph/file/0169f000c9ddc7c3315ff.jpg',
  confess: 'https://telegra.ph/file/03cabea082a122abfa5be.jpg',
@@ -46,9 +46,9 @@ global.media = {
 
 // Sosmed
 global.url = {
- sig: 'https://instagram.com/noureddine_ouafy',
+ sig: 'https://www.instagram.com/flash.xiiit?igsh=ZmJpczJ3a3IyaXZo',
  sgh:  'https://github.com/noureddineouafy',
- sgc: 'https://chat.whatsapp.com/DABxracGsi5CQMd5d1N1j7'
+ sgc: 'https://wa.me/+212714249761'
 }
 
 global.wait =` انتظر .. أنا أحاول تلبية طلبك ...`
@@ -56,7 +56,7 @@ global.wait =` انتظر .. أنا أحاول تلبية طلبك ...`
 // Info Wait
 global.msg = {
  wait: '⏱️ *Please be patient*\n\> Running command from *User*!',
- eror: '🤖*Bot Information*\n\> Sorry for the inconvenience in using *Bobiza Bot*. There was an error in the system while executing the command.'
+ eror: '🤖*Bot Information*\n\> Sorry for the inconvenience in using *Flx Bot*. There was an error in the system while executing the command.'
 }
 
 global.multiplier = 69
